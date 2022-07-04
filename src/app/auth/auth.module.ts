b600096f2744c40695124f829d1f787a3fa35ddc
Pedro Alvarez <paaf170893@gmail.com>
2022-07-04 00:16:29 -0400

    Primer commit, agregando primeras rutas

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+
+import { RegistroComponent } from './pages/registro/registro.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ForgotComponent } from './pages/forgot/forgot.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    RegistroComponent,
+    LoginComponent,
+    ForgotComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule { }
